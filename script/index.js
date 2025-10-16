ccf6fa86ec11626000012be824d4fddb31354f70
nothing36 <prodipsarker953@gmail.com>
2025-10-16 11:30:22 +0600

    category updated

--- script/index.js
+++ script/index.js
@@ -1,1 +1,25 @@
-console.log("Js is connected");
+
+
+function loadCategories(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => (res.json()))
+    .then((data) => (displayCategories(data.categories)) )
+}
+
+function displayCategories(data){
+    const category_container=document.getElementById('category-container');
+
+for(const item of data){
+    
+    const category_div=document.createElement('div');
+    category_div.innerHTML=`
+    <button class="btn btn-sm">${item.category}</button>
+    `
+    category_container.appendChild(category_div);
+}
+}
+
+
+
+
+loadCategories();
