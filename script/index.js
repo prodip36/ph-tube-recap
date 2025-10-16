9a3c5cabda2199d1dd62f02ee8ce235f052c75b8
nothing36 <prodipsarker953@gmail.com>
2025-10-16 13:29:53 +0600

    working on videos

--- script/index.js
+++ script/index.js
@@ -1,25 +1,33 @@
-
-
-function loadCategories(){
-    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    .then((res) => (res.json()))
-    .then((data) => (displayCategories(data.categories)) )
+function loadCategories() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories));
+  
+}
+// load videos
+function loadVideos() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos));
 }
 
-function displayCategories(data){
-    const category_container=document.getElementById('category-container');
-
-for(const item of data){
-    
-    const category_div=document.createElement('div');
-    category_div.innerHTML=`
-    <button class="btn btn-sm">${item.category}</button>
+function displayCategories(data) {
+  const category_container = document.getElementById("category-container");
+  for (const item of data) {
+    const category_div = document.createElement("div");
+    category_div.innerHTML = `
+    <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${item.category}</button>
     `
     category_container.appendChild(category_div);
+  }
 }
+
+function displayVideos(videos){
+   const display_videos = document.getElementById("display-videos");
+    videos.forEach(video =>{
+      console.log(video);
+    })
 }
-
-
-
 
 loadCategories();
+loadVideos();
