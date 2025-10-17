c26fb1da2969838c0851916073f2acb69c80ec79
nothing36 <prodipsarker953@gmail.com>
2025-10-18 00:56:27 +0600

    card section

--- script/index.js
+++ script/index.js
@@ -2,7 +2,6 @@ function loadCategories() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
     .then((data) => displayCategories(data.categories));
-  
 }
 // load videos
 function loadVideos() {
@@ -17,35 +16,49 @@ function displayCategories(data) {
     const category_div = document.createElement("div");
     category_div.innerHTML = `
     <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${item.category}</button>
-    `
+    `;
     category_container.appendChild(category_div);
   }
 }
 
-function displayVideos(videos){
-   const display_videos = document.getElementById("display-videos");
-    videos.forEach(video =>{
-      const div_videos=document.createElement('div');
-      div_videos.innerHTML = `
-      <div class="card bg-base-100 shadow-sm">
-  <figure class="h-[200px]">
-    <img
-      src="${video.thumbnail}"
-      alt="Shoes"
-      class="h-full w-full rounded-xl" />
-  </figure>
-  <div class="card-body items-center text-center">
-    <h2 class="card-title">${video.title}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+function displayVideos(videos) {
+  const display_videos = document.getElementById("display-videos");
+  videos.forEach((video) => {
+    const div_videos = document.createElement("div");
+    div_videos.innerHTML = `
+    <div class="card bg-base-100">
+        <figure class="relative h-[200px]">
+          <img
+            src="${video.thumbnail}"
+            alt="Shoes"
+            class="h-full w-full rounded-xl"
+          />
+          <span
+            class="absolute bottom-2 right-2 bg-black text-white text-sm rounded px-1"
+          >
+            3hrs 56 min ago
+          </span>
+        </figure>
+        <div class="flex gap-3 py-5">
+          <!-- profile pic -->
+          <div class="">
+            <div class="avatar">
+              <div class="ring-primary ring-offset-base-100 w-10 rounded-full">
+                <img src="${video.authors[0].profile_picture}" />
+              </div>
+            </div>
+          </div>
+          <!-- description --> 
+          <div class=" space-y-3">
+            <h2 class="font-bold text-xl">${video.title}</h2>
+            <p class="text text-gray-400 flex gap-1 items-center">${video.authors[0].profile_name} <img class="h-5 w-5" src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000" alt=""></p>
+            <p class="text text-gray-400">${video.others.views}</p>
+          </div>
+        </div>
+      </div>
       `;
-      display_videos.appendChild(div_videos);
-
-    })
+    display_videos.appendChild(div_videos);
+  });
 }
 
 loadCategories();
